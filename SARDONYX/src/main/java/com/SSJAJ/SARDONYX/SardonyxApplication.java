bdbbcc03b0ce798fd38dc79ec8139df8546b470b
Prashant Kumar <prashantkumarptb@gmail.com>
2023-11-30 07:55:14 +0530

    Initial commit

---
+++ SARDONYX/src/main/java/com/SSJAJ/SARDONYX/SardonyxApplication.java
@@ -0,0 +1,13 @@
+package com.SSJAJ.SARDONYX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SardonyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SardonyxApplication.class, args);
+	}
+
+}
---
+++ SARDONYX/src/test/java/com/SSJAJ/SARDONYX/SardonyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSJAJ.SARDONYX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SardonyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
